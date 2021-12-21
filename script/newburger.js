a44b0823035201886654438b51ba7c7391dad8e7
andychen00 <andychen9979@gmail.com>
2021-12-21 13:48:26 +0700

    Canvas Selesai

--- script/newburger.js
+++ script/newburger.js
@@ -2,9 +2,9 @@ var x = 0;
 var y = 0;
 var velox = 2;
 var veloy = 3;
-var canvas_width = 300;
-var canvas_height = 300;
-var boxsize = 50;
+var canvas_width = 900;
+var canvas_height = 400;
+var boxsize = 100;
 
 var canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
@@ -12,15 +12,15 @@ anim_img = new Image(0, 0);
 anim_img.onload = function () {
     setInterval(() => {
         ctx.clearRect(0, 0, canvas_width, canvas_height);
-        if (x < 0 || x + boxsize > canvas_width) { 
-            velox *= -1; 
-        }
-        if (y < 0 || y + boxsize > canvas_height) { 
-            veloy *= -1; 
-        }
+        ctx.drawImage(anim_img, x, y, boxsize, boxsize);
         x += velox;
         y += veloy;
-        ctx.drawImage(anim_img, x, y, boxsize, boxsize);
+        if (x <= 0 || x + boxsize >= canvas_width) {
+            velox *= -1;
+        }
+        if (y <= 0 || y + boxsize >= canvas_height) {
+            veloy *= -1;
+        }
     }, 10);
-}
-anim_img.src = 'assets/bouncing-burger.jpg';
+};
+anim_img.src = "assets/bouncing-burger.jpg";
